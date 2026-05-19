d58839a99f9e77cdcb910fe7bdbcd9825eb16e21
Pedro Faviny Osorio Varela <pedro.osorio@genstudents.org>
2026-05-19 16:46:15 -0500

    Proyecto final platos típicos colombianos

--- main.py
+++ main.py
@@ -1,5 +1,67 @@
-def main():
-  print("Hello learners!")
+import json
 
-if __name__=="__main__":
-  main()
+
+def dish_fetch(id):
+
+    datos_json = """
+    [
+        {
+            "id": 1,
+            "name": "Bandeja Paisa",
+            "region": "Antioquia"
+        },
+
+        {
+            "id": 5,
+            "name": "Ajiaco",
+            "region": "Bogotá"
+        },
+
+        {
+            "id": 15,
+            "name": "Lechona",
+            "region": "Tolima"
+        }
+    ]
+    """
+
+    platos = json.loads(datos_json)
+
+    for plato in platos:
+
+        if plato["id"] == id:
+
+            return plato
+
+
+def mostrar_menu():
+
+    print("\n===== PLATOS TÍPICOS COLOMBIANOS =====\n")
+
+    print("1 - Bandeja Paisa")
+    print("5 - Ajiaco")
+    print("15 - Lechona")
+
+    opcion = int(input("\nSeleccione un plato: "))
+
+    plato = dish_fetch(opcion)
+
+    if plato:
+
+        print("\n" + "*" * 20)
+
+        print("Nombre:", plato["name"])
+        print("Región:", plato["region"])
+
+    else:
+
+        print("Opción inválida")
+
+
+def main():
+
+    mostrar_menu()
+
+
+if __name__ == "__main__":
+    main()
